01711a5bde9460367131cf028e9e956f685c05f4
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-12-26 12:23:53 -0800

    Create minimal_example.py

---
+++ minimal_example.py
@@ -0,0 +1,27 @@
+import pytorch_lightning as pl
+
+
+def main():
+    train_loader = DataLoader(MNIST(os.getcwd(), download=True, transform=transforms.ToTensor()))
+    trainer = pl.Trainer()
+    model = LitModel()
+    trainer.fit(model, train_loader)
+
+
+class LitModel(pl.LightningModule):
+
+     def __init__(self):
+         super().__init__()
+         self.l1 = torch.nn.Linear(28 * 28, 10)
+
+     def forward(self, x):
+         return torch.relu(self.l1(x.view(x.size(0), -1)))
+
+     def training_step(self, batch, batch_idx):
+         x, y = batch
+         y_hat = self(x)
+         loss = F.cross_entropy(y_hat, y)
+         return loss
+
+     def configure_optimizers(self):
+         return torch.optim.Adam(self.parameters(), lr=0.02)
